7a7106cb456a1456ad83a61e39d1ba4b9bde7ea8
nmaksy <naty.maksymiv@gmail.com>
2024-10-14 11:50:26 +0300

    homework 4 added

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const myName = 'Natalie';
+const myDaughterName = 'Mariia';
+const generalGreeting = 'Hello, ';
+const myPersonalGreeting = `${generalGreeting}${myName}!`;
+const daughterPersonalGreeting = generalGreeting + myDaughterName +'!';
+
+console.log(myPersonalGreeting);
+console.log(daughterPersonalGreeting);
---
+++ jsBasics/task3.js
@@ -0,0 +1,9 @@
+
+let age = 30; 
+
+
+let isAdult = age >= 18;
+
+
+console.log("Age:", age);
+console.log("Is Adult:", isAdult);
