ca7f9d1198f05b8915aa0762a30db0acabae992b
NataliMaksymiv <76060427+NataliMaksymiv@users.noreply.github.com>
2024-10-14 15:08:30 +0300

    Updated task2.js

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -3,6 +3,5 @@ const myDaughterName = 'Mariia';
 const generalGreeting = 'Hello, ';
 const myPersonalGreeting = `${generalGreeting}${myName}!`;
 const daughterPersonalGreeting = generalGreeting + myDaughterName +'!';
-
 console.log(myPersonalGreeting);
 console.log(daughterPersonalGreeting);
